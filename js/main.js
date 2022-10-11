bcf34fdef7f68c700f66cd0fc739d73e792c4527
matiasdamianarevalo <matiasdamianarevalo@gmail.com>
2022-10-11 20:31:35 -0300

    fetch

--- js/main.js
+++ js/main.js
@@ -1,10 +1,122 @@
-import { homeControllers } from 'js/homeControllers.js';
+
+
+
+
+const bicicletas = [
+    {
+        id: 1,
+        nombre: "SUPERCALIBER 9.9",
+        img: "img/SUPERCALIBER 9.9.jpg",
+        precio: 1000,
+        cantidad: 1,
+        stock: 1,
+    },
+    {
+        id: 2,
+        nombre: "SUPERCALIBER 9.8",
+        img: "img/SUPERCALIBER 9.8.jpg",
+        precio: 970,
+        cantidad: 1,
+        stock: 6,
+    },
+    {
+        id: 3,
+        nombre: "SUPERCALIBER 9.7",
+        img: "img/SUPERCALIBER 9.7.jpg",
+        precio: 930,
+        cantidad: 1,
+        stock: 0,
+    },
+    {
+        id: 4,
+        nombre: "FUEL EX 7",
+        img: "img/FUEL EX 7.jpg",
+        precio: 1100,
+        cantidad: 1,
+        stock: 7, 
+    },
+    {
+        id: 5,
+        nombre: "TOP FUEL 9.9",
+        img: "img/TOP FUEL 9.9.jpg",
+        precio: 1180,
+        cantidad: 1,
+        stock: 0, 
+    },
+    {
+        id: 6,
+        nombre: "TOP FUEL 9.8",
+        img: "img/TOP FUEL 9.8.jpg",
+        precio: 1100,
+        cantidad: 1,
+        stock: 75, 
+    },
+    {
+        id: 7,
+        nombre: "TOP FUEL 9.7",
+        img: "img/TOP FUEL 9.7.jpg",
+        precio: 1000,
+        cantidad: 1,
+        stock: 4, 
+    },
+    {
+        id: 8,
+        nombre: "TOP FUEL 8",
+        img: "img/TOP FUEL 8.jpg",
+        precio: 1000,
+        cantidad: 1,
+        stock: 1, 
+    },
+    {
+        id: 9,
+        nombre: "PROCALIBER 9.8",
+        img: "img/PROCALIBER 9.8.jpg",
+        precio: 900,
+        cantidad: 1,
+        stock: 3,
+    },
+    {
+        id: 10,
+        nombre: "PROCALIBER 9.7",
+        img: "img/PROCALIBER 9.7.jpg",
+        precio: 900,
+        cantidad: 1,
+        stock: 2,
+    },
+    {
+        id: 11,
+        nombre: "PROCALIBER 9.6",
+        img: "img/PROCALIBER 9.6.jpg",
+        precio: 950,
+        cantidad: 1,
+        stock: 2,
+    },
+    {
+        id: 12,
+        nombre: "PROCALIBER 9.5",
+        img: "img/PROCALIBER 9.5.jpg",
+        precio: 850,
+        cantidad: 1,
+        stock: 4,
+    },
+];
+
+const homeControllers = async () => {
+    try{
+        const response = await fetch ('src/stock.json');
+        const data = await response.json();
+        
+        return data;
+    }catch(error){
+        console.log('hubo un error', error);
+    }
+};
   
 document.addEventListener('DOMContentLoaded', async() => {
     
     const bicicletas = await homeControllers();
 
-    renderizarProductos(bicicletas);
+    renderizarProductos();
 
     if (localStorage.getItem('carrito')) {
         const carrito = obtenerCarritoStorage();
@@ -12,7 +124,7 @@ document.addEventListener('DOMContentLoaded', async() => {
         calcularTotal(carrito);
     }
 });
-renderizarProductos();
+
 
 
 /////operador  or ///
@@ -23,7 +135,7 @@ const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 function renderizarProductos(){
 
     const tienda = document.getElementById('tienda');
-    /// destructure//
+    
     bicicletas.forEach(({img,nombre, precio, id}) => {
 
         let producto = document.createElement('div');
